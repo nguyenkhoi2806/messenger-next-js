b865d1a091993bd215a876a5f246e7db55bfd294
nguyenkhoi2806 <nhunguyennq@nashtechglobal.com>
2023-07-27 23:00:46 +0700

    update code today

---
+++ app/(site)/AuthForm/index.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useCallback, useState } from "react";
+
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+    if (variant === "REGISTER") {
+    }
+
+    if (variant === "LOGIN") {
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setLoading(true);
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}></form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
