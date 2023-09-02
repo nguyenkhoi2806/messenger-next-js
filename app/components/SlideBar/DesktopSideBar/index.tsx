d8942c8f61079ae4f02cd3f12fa82eea5cc3f4fb
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2023-09-02 10:15:55 +0700

    update code

--- app/components/SlideBar/DesktopSideBar/index.tsx
+++ app/components/SlideBar/DesktopSideBar/index.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import useRoute from "@/app/hooks/useRoute";
+import { User } from "@prisma/client";
 import { useState } from "react";
+import Avatar from "../../Avatar";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSideBar = () => {
+interface DesktopSideBarProps {
+  currentUser: User;
+}
+
+const DesktopSideBar = (props: DesktopSideBarProps) => {
   const routes = useRoute();
   const [isOpen, setIsOpen] = useState(false);
+  const { currentUser } = props;
 
   return (
-    <div className="hidden lg:fixed lg:inset-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:col lg:justify-between">
+    <div className="hidden lg:fixed lg:inset-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col lg:justify-between">
       <nav className="mt-4 flex flex-col justify-between">
         <ul role="list" className="flex flex-col items-center space-y-1">
           {routes.map((item) => (
@@ -23,6 +30,14 @@ const DesktopSideBar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
--- app/components/SlideBar/index.tsx
+++ app/components/SlideBar/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import DesktopSideBar from "./DesktopSideBar";
 import MobileFooter from "./MobileFooter";
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
 
-const SideBar = ({ children }: { children: React.ReactNode }) => {
+const SideBar = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
   return (
     <div className="h-full">
-      <DesktopSideBar />
+      <DesktopSideBar currentUser={currentUser!} />
       <MobileFooter />
       <main className="lg-pl-20 h-full">{children}</main>
     </div>
