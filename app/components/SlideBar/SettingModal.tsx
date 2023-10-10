63afd5a4a998fcc4ac733d99bed3f8c0357f956a
khoilen <khoilen@nashtechglobal.com>
2023-10-10 22:39:05 +0700

    Add setting modals

--- app/components/SlideBar/SettingModal.tsx
+++ app/components/SlideBar/SettingModal.tsx
@@ -6,7 +6,11 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-import Modal from "../Modal";
+import Modal from "../Modal"; 
+import Input from "../Input";
+import Image from "next/image";
+import { CldUploadButton } from "next-cloudinary";
+import Button from "../Button";
 
 interface SettingModalProps {
   currentUser: User;
@@ -50,6 +54,8 @@ const SettingModal = (props: SettingModalProps) => {
       })
       .catch(() => {
         toast.error("Something went wrong");
+      }).finally(() => {
+        setIsLoading(false)
       });
   };
 
@@ -61,8 +67,47 @@ const SettingModal = (props: SettingModalProps) => {
             <h2 className="text-base font-semibold leading-7 text-gray-900">
               Profile
             </h2>
-            <p className="mt-1 text-sm leading-6"></p>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Edit your public information
+            </p>
+            <div className="mt-10 flex flex-col gap-y-8">
+              <Input 
+                disabled={isLoading}
+                label="Name"
+                id="name"
+                errors={errors}
+                required
+                register={register} 
+              />
+              <div>
+                <label className="blog text-sm font-medium leading-6 text-gray-900">
+                  Photo
+                </label>
+                <div className="mt-2 flex items-center gap-x-3">
+                <Image 
+                  width="48"
+                  height="48" 
+                  className="rounded-full"
+                  src={image || currentUser.image || '/images/placeholder.jpg'}
+                  alt="Avatar"
+                />
+                <CldUploadButton options={{maxFiles: 1}} onUpload={handleUpload} uploadPreset="stxugkvm">
+                  <Button disabled={isLoading} secondary type="button">
+                      Change
+                  </Button>
+                </CldUploadButton>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+        <Button disabled={isLoading} secondary onClick={onClose}>
+          Cancel
+        </Button>
+        <Button disabled={isLoading} type="submit" >
+          Save
+        </Button>
         </div>
       </form>
     </Modal>
